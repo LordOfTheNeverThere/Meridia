ec294fb509580215ac1c1bcb67e960a1eff25fa2
LordOfTheNeverThere <miguel.dinis.s@tecnico.ulisboa.pt>
2026-05-05 10:50:25 +0100

    Finish tests for user creation

--- meridia/src/test/java/com/whitetower/meridia/UserIntegrationTests.java
+++ meridia/src/test/java/com/whitetower/meridia/UserIntegrationTests.java
@@ -1,22 +1,27 @@
 package com.whitetower.meridia;
 import com.whitetower.meridia.controller.UserController;
 
+import com.whitetower.meridia.dto.UserRegistrationDTO;
 import com.whitetower.meridia.model.User;
 import com.whitetower.meridia.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
+import org.mockito.Mockito;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
+import static org.mockito.Mockito.mockConstruction;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -42,6 +47,37 @@ public class UserIntegrationTests {
 
         List<User> allUsers =  db.findAll();
         Assertions.assertEquals(1, allUsers.size());
+    }
+
+    @Test
+    void creationOfDuplicateUser() throws Exception {
+
+        String requestJson = Files.readString(Path.of("src/test/resources/requests/userController/createUser.json"));
+        String response = mockMvc.perform(post(UserController.API_USER_POST).contentType(MediaType.APPLICATION_JSON).content(requestJson))
+                .andExpect(status().isCreated()).andReturn().getResponse().getContentAsString();
+        String expectedResponse = Files.readString(Path.of("src/test/resources/responses/userController/createUser.json"));
+
+        JSONAssert.assertEquals(expectedResponse, response, JSONCompareMode.STRICT);
+
+        response = mockMvc.perform(post(UserController.API_USER_POST).contentType(MediaType.APPLICATION_JSON).content(requestJson))
+            .andExpect(status().isBadRequest()).andReturn().getResponse().getContentAsString();
+
+        expectedResponse = Files.readString(Path.of("src/test/resources/responses/userController/entityAlreadyExists.json"));
+        JSONAssert.assertEquals(expectedResponse, response, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    void creationOfDuplicateUserWithDBConstraintViolation() throws Exception {
+
+        try (MockedConstruction<User> mocked = mockConstruction(User.class, (mock, context) -> {
+        })) {
+            String requestJson = Files.readString(Path.of("src/test/resources/requests/userController/createUser.json"));
+            String response = mockMvc.perform(post(UserController.API_USER_POST).contentType(MediaType.APPLICATION_JSON).content(requestJson))
+                    .andExpect(status().isBadRequest()).andReturn().getResponse().getContentAsString();
+            String expectedResponse = Files.readString(Path.of("src/test/resources/responses/userController/invalidEntity.json"));
+
+            JSONAssert.assertEquals(expectedResponse, response, JSONCompareMode.STRICT);
+        }
     }
 
 }
